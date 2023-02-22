667a7da076da1375e7b33130c869ea2d1124eebf
ScpBill <122083220+ScpBill@users.noreply.github.com>
2023-02-22 12:01:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('GitHub')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# See GitHub help at https://www.jetbrains.com/help/pycharm/
